129cfb5c29adb5a76141773df765df8e0f1f4eb8
Markus Korporal <markus@korporal.de>
2021-03-18 16:54:20 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,7 +2,3 @@
 use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::LIBRARY, 'zuckerwelt/dpdcloudapi', __DIR__);
-
-if (!function_exists('__')) {
-    require 'Phrase/__.php';
-}
